d31827f28c4bb5f3775988218b4b5b83e1015269
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-04-08 20:50:40 +0000

    feat: parser working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use grrs::{error, flags::parser::get_args};
+
 fn main() {
-    println!("Hello, world!");
+    let args = get_args();
+
+    let content = match std::fs::read_to_string(&args.path) {
+        Ok(content) => content,
+        Err(err) => {
+            error!("Error: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    for (i, line) in content.lines().enumerate() {
+        if line.contains(&args.pattern) {
+            println!("{}\t{}", i, line);
+        }
+    }
 }
