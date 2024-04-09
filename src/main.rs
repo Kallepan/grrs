38d9a1a15831c7abf189046eef9cf5620cac4b0a
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-04-09 14:43:19 +0000

    feat: refactored find_matches

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use grrs::{error, flags::parser::get_args};
 
+fn find_matches(content: String, pattern: &str, mut writer: impl std::io::Write) {
+    for (_, line) in content.lines().enumerate() {
+        if !line.contains(pattern) {
+            continue;
+        }
+
+        match writeln!(writer, "{}", line) {
+            Ok(_) => {}
+            Err(err) => {
+                error!("Could not write to output: {}", err);
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
 fn main() {
     let args = get_args();
 
@@ -11,9 +27,16 @@ fn main() {
         }
     };
 
-    for (i, line) in content.lines().enumerate() {
-        if line.contains(&args.pattern) {
-            println!("{}\t{}", i, line);
-        }
-    }
+    find_matches(content, &args.pattern, &mut std::io::stdout());
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches(
+        String::from("lorem ipsum\ndolor sit amet"),
+        "lorem",
+        &mut result,
+    );
+    assert_eq!(result, b"lorem ipsum\n");
 }
